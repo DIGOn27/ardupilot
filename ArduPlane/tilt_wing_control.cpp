c596b768be1f6455e817af13ca4552c3750b91c4
DIGOn27 <diogopenu@gmail.com>
2026-05-26 18:18:44 +0100

    get_desired_yaw_rate in tilt wing control yaw input

--- ArduPlane/tilt_wing_control.cpp
+++ ArduPlane/tilt_wing_control.cpp
@@ -41,7 +41,7 @@ void Plane::set_tilt_wing_out()
 
         //yaw input 
         //ADD parameter to define max wing deflection for yaw
-        float rudder = SRV_Channels::get_output_scaled(SRV_Channel::k_rudder) ;///45 *g.tilt_wing_yaw_max; //scale rudder input to max wing deflection for yaw
+        float yaw_output = quadplane.get_desired_yaw_rate_cds(true) / quadplane.command_model_pilot.get_rate() * g.tilt_wing_yaw_max*100;//SRV_Channels::get_output_scaled(SRV_Channel::k_rudder) ;///45 *g.tilt_wing_yaw_max; //scale rudder input to max wing deflection for yaw
 
         float front_wing_ang = SRV_Channels::get_slew_limited_output_scaled(SRV_Channel::k_front_wing_tilt);
         float back_wing_ang = SRV_Channels::get_slew_limited_output_scaled(SRV_Channel::k_back_wing_tilt);
@@ -53,8 +53,8 @@ void Plane::set_tilt_wing_out()
             back_wing_ang = back_wing_out;
         }
 
-        front_wing_out  = constrain_float(rudder + front_wing_out, -4500, 4500);
-        back_wing_out = constrain_float(-rudder + back_wing_out, -4500, 4500);
+        front_wing_out  = constrain_float(yaw_output + front_wing_out, -4500, 4500);
+        back_wing_out = constrain_float(-yaw_output + back_wing_out, -4500, 4500);
 
         SRV_Channels::set_output_scaled(SRV_Channel::k_front_wing_tilt, front_wing_out);
         SRV_Channels::set_output_scaled(SRV_Channel::k_back_wing_tilt, back_wing_out);
